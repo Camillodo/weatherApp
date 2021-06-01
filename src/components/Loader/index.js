1063b8e9656a2ed19019b0e91389a10a81d785d1
Camillodo <camille.doizelet@gmail.com>
2021-06-01 17:12:26 +0200

    loader added

---
+++ src/components/Loader/index.js
@@ -0,0 +1,15 @@
+/* eslint-disable no-console */
+// == Import npm
+import React from 'react';
+
+// == Import
+import './loader.scss';
+
+// == Composant
+
+const Loader = () => (
+  <div className="loader" />
+);
+
+// == Export
+export default Loader;
--- src/components/WeatherCard/index.js
+++ src/components/WeatherCard/index.js
@@ -5,6 +5,7 @@ import axios from 'axios';
 import moment from 'moment';
 // == Import
 import './weather-card.scss';
+import Loader from 'src/components/Loader';
 import humidity from 'src/assets/img/humidity.svg';
 import wind from 'src/assets/img/wind.svg';
 import cloud from 'src/assets/img/cloud.svg';
@@ -15,9 +16,10 @@ const WeatherCard = () => {
   const [forecastSpecs, setForecastSpecs] = useState([]);
   const [windSpecs, setWind] = useState([]);
   const [cloudSpecs, setCloud] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   const loadWeatherInfo = () => {
-    const weatherInfoURL = 'http://api.openweathermap.org/data/2.5/weather?q=Paris,fr&units=metric&APPID=GETYOUROWNATOPENWEATHER';
+    const weatherInfoURL = 'http://api.openweathermap.org/data/2.5/weather?q=Paris,fr&units=metric&APPID=GETYOUROWNAThttps://openweathermap.org/current';
     axios.get(weatherInfoURL)
       .then((res) => {
         setForecast(res.data.main);
@@ -27,6 +29,9 @@ const WeatherCard = () => {
       })
       .catch((err) => {
         console.error(err);
+      })
+      .finally(() => {
+        setLoading(false);
       });
   };
 
@@ -38,31 +43,36 @@ const WeatherCard = () => {
   const today = moment().format('L');
   return (
     <div className="weather__card">
-      <div className="weather__card__temp__info">
-        <div className="weather__card__main__info">
-          <h1 className="weather__card__main__date">Paris<span className="weather__card__main__date--full">{today}</span></h1>
-          <h2 className="weather__card__main__temperature">{Math.round(forecast.temp)}<span className="weather__card__felt__temperature"> /{Math.round(forecast.temp_min)} <sup className="weather__card__sup">℃</sup></span>
-          </h2>
+      {loading && <Loader />}
+      {!loading && (
+      <article className="main_card">
+        <div className="weather__card__temp__info">
+          <div className="weather__card__main__info">
+            <h1 className="weather__card__main__date">Paris<span className="weather__card__main__date--full">{today}</span></h1>
+            <h2 className="weather__card__main__temperature">{Math.round(forecast.temp)}<span className="weather__card__felt__temperature"> /{Math.round(forecast.temp_min)} <sup className="weather__card__sup">℃</sup></span>
+            </h2>
+          </div>
+          <div className="weather__card__temp__icon">
+            <img src={icon} alt="icon" className="weather__card__icon__weather__type" />
+            <h2 className="weather__card__description">{forecastSpecs.main}</h2>
+          </div>
         </div>
-        <div className="weather__card__temp__icon">
-          <img src={icon} alt="icon" className="weather__card__icon__weather__type" />
-          <h2 className="weather__card__description">{forecastSpecs.main}</h2>
+        <div className="weather__card__weatherspecs">
+          <div className="weather__card__weatherspecs__container">
+            <img src={wind} alt="wind__icon" className="weather__card__weatherspecs__icon" />
+            <p>{Math.round(windSpecs.speed)} km/h</p>
+          </div>
+          <div className="weather__card__weatherspecs__container">
+            <img src={humidity} alt="humidity__icon" className="weather__card__weatherspecs__icon" />
+            <p>{forecast.humidity} %</p>
+          </div>
+          <div className="weather__card__weatherspecs__container">
+            <img src={cloud} alt="cloud__icon" className="weather__card__weatherspecs__icon" />
+            <p>{cloudSpecs.all}</p>
+          </div>
         </div>
-      </div>
-      <div className="weather__card__weatherspecs">
-        <div className="weather__card__weatherspecs__container">
-          <img src={wind} alt="wind__icon" className="weather__card__weatherspecs__icon" />
-          <p>{Math.round(windSpecs.speed)} km/h</p>
-        </div>
-        <div className="weather__card__weatherspecs__container">
-          <img src={humidity} alt="humidity__icon" className="weather__card__weatherspecs__icon" />
-          <p>{forecast.humidity} %</p>
-        </div>
-        <div className="weather__card__weatherspecs__container">
-          <img src={cloud} alt="cloud__icon" className="weather__card__weatherspecs__icon" />
-          <p>{cloudSpecs.all}</p>
-        </div>
-      </div>
+      </article>
+      )}
     </div>
 
   );
